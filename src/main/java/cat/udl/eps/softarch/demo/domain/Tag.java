4191c60809c30d9147b45ad5d8886b75d751d73c
Joel Sambola <joelsam388@gmail.com>
2026-02-19 16:39:56 +0100

    Updated tag

--- src/main/java/cat/udl/eps/softarch/demo/domain/Tag.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Tag.java
@@ -1,5 +1,9 @@
 package cat.udl.eps.softarch.demo.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -8,16 +12,23 @@ import lombok.Setter;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Tag {
+public class Tag extends UriEntity<Long> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @NotBlank
     @EqualsAndHashCode.Include
     private String name;
 
     private Set<Project> projects = new HashSet<>();
+
+    public Tag() {}
 
     public Tag(String name) {
         this.name = name;
