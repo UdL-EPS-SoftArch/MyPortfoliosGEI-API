3724cbbd1891920162a715e9c6fd89587ed632e4
jonsson11 <jooonds11@gmail.com>
2026-02-19 16:02:06 +0100

    Tag implemented
    Methods added.

--- src/main/java/cat/udl/eps/softarch/demo/domain/Tag.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Tag.java
@@ -1,2 +1,49 @@
-package cat.udl.eps.softarch.demo.domain;public class Tag {
+package cat.udl.eps.softarch.demo.domain;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Tag {
+
+    private String name;
+    private Set<Project> projects = new HashSet<>();
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public Set<Project> getProjects(){
+        return projects;
+    }
+
+    public void addProject(Project project){
+        projects.add(project);
+    }
+
+    public void removeProject(Project project){
+        projects.remove(project);
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (!(o instanceof Tag)) return false;
+        Tag tag = (Tag) o;
+        return Objects.equals(name, tag.name);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hashCode(name);
+    }
+
 }
