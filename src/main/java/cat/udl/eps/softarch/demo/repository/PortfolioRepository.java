8770fba5487e9e2b2f463ccecbc742ee1c8a6b6f
AngelLopez23 <alm39@alumnes.udl.cat>
2026-02-25 18:11:50 +0100

    Added extends CrudRepository to interface PortfolioRepository

--- src/main/java/cat/udl/eps/softarch/demo/repository/PortfolioRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/PortfolioRepository.java
@@ -3,6 +3,8 @@ package cat.udl.eps.softarch.demo.repository;
 import cat.udl.eps.softarch.demo.domain.Portfolio;
 import cat.udl.eps.softarch.demo.domain.User;
 import cat.udl.eps.softarch.demo.domain.Visibility;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -10,7 +12,7 @@ import java.util.List;
 
 
 @RepositoryRestResource
-public interface PortfolioRepository {
+public interface PortfolioRepository extends CrudRepository<Portfolio, Long>, PagingAndSortingRepository<Portfolio, Long> {
     List<Portfolio> findByCreator(@Param("user") User creator);
     List<Portfolio> findByNameContaining(@Param("name") String text);
     List<Portfolio> findByVisibility(@Param("visibility") Visibility visibility);
