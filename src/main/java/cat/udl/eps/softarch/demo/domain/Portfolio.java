4efcad0dd111fa0d5cee16b247e47ec2b6b491c6
Marc Lapeña Riu <jakoncoach@gmail.com>
2026-03-03 16:29:28 +0100

    Feat: Status variable for Project

--- src/main/java/cat/udl/eps/softarch/demo/domain/Portfolio.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Portfolio.java
@@ -33,6 +33,10 @@ public class Portfolio extends UriEntity<Long> {
     private Visibility visibility;
 
     @Setter
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Setter
     @ManyToOne
     @JsonIdentityReference(alwaysAsId = true)
     private User creator;
