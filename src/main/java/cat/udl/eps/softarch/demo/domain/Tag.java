212efd3aeb0c6f0e90370088e8e2c824bb785bc7
jonsson11 <jooonds11@gmail.com>
2026-03-12 15:08:30 +0100

    Add basic Tag entity (without Project relation)

--- src/main/java/cat/udl/eps/softarch/demo/domain/Tag.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Tag.java
@@ -1,14 +1,10 @@
 package cat.udl.eps.softarch.demo.domain;
 
-import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Getter
@@ -21,32 +17,14 @@ public class Tag extends UriEntity<Long> {
     private Long id;
 
     @NotBlank
-    @Column(nullable = false, unique = true)
     @EqualsAndHashCode.Include
+    @Column(nullable = false, unique = true)
     private String name;
-
-    // --- Relationships ---
-
-    @ManyToMany(mappedBy = "tags")
-    @JsonIdentityReference(alwaysAsId = true)
-    private Set<Project> projects = new HashSet<>();
 
     public Tag() {}
 
     public Tag(String name) {
         this.name = name;
-    }
-
-    // --- Helper methods ---
-
-    public void addProject(Project project) {
-        projects.add(project);
-        project.getTags().add(this);
-    }
-
-    public void removeProject(Project project) {
-        projects.remove(project);
-        project.getTags().remove(this);
     }
 
     @Override
