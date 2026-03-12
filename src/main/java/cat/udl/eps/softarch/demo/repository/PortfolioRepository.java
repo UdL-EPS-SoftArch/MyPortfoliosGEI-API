5bb4bbd9b071bd41c8a0927faf30f1e3c5ffab63
maeldutrey <mael.dutrey05@gmail.com>
2026-03-12 16:27:19 +0100

    add paginated search for portfolios by name

--- src/main/java/cat/udl/eps/softarch/demo/repository/PortfolioRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/PortfolioRepository.java
@@ -3,6 +3,8 @@ package cat.udl.eps.softarch.demo.repository;
 import cat.udl.eps.softarch.demo.domain.Portfolio;
 import cat.udl.eps.softarch.demo.domain.User;
 import cat.udl.eps.softarch.demo.domain.Visibility;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,13 +12,17 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-
 @RepositoryRestResource
 public interface PortfolioRepository extends CrudRepository<Portfolio, Long>, PagingAndSortingRepository<Portfolio, Long> {
 
     List<Portfolio> findByCreator(@Param("user") User creator);
 
     List<Portfolio> findByNameContaining(@Param("name") String text);
+
+    Page<Portfolio> findByNameContaining(
+        @Param("name") String text,
+        Pageable pageable
+    );
 
     List<Portfolio> findByVisibility(@Param("visibility") Visibility visibility);
 
