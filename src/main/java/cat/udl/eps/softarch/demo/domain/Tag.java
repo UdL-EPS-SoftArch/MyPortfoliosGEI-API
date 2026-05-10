5ae96ce153194d961c21e6e5378348fd81c904cb
jonsson11 <jooonds11@gmail.com>
2026-05-10 10:10:56 +0200

    Final Tag.java

--- src/main/java/cat/udl/eps/softarch/demo/domain/Tag.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Tag.java
@@ -1,12 +1,13 @@
 package cat.udl.eps.softarch.demo.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,19 +17,22 @@ import java.util.Set;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Tag extends UriEntity<Long> {
 
-    @ManyToMany(mappedBy = "tags")
-    @JsonIgnore
-    private Set<Record> records = new HashSet<>();
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank
     @EqualsAndHashCode.Include
-    @Pattern(regexp = "^[a-zA-Z0-9 ]+$", message = "Only alphanumeric characters and spaces are allowed")
-    @Column(unique = true)
+    @Pattern(
+        regexp = "^[a-zA-Z0-9 ]+$",
+        message = "Only alphanumeric characters and spaces are allowed"
+    )
+    @Column(unique = true, nullable = false)
     private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    @JsonIgnore
+    private Set<Record> records = new HashSet<>();
 
     public Tag() {}
 
@@ -39,9 +43,5 @@ public class Tag extends UriEntity<Long> {
     @Override
     public Long getId() {
         return id;
-    }
-
-    public String getName() {
-        return name;
     }
 }
