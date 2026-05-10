3aa907890585f00e31cf2a619d940d62f56ddcc9
jonsson11 <jooonds11@gmail.com>
2026-05-10 14:27:31 +0200

    Database initialization fixed

--- src/main/java/cat/udl/eps/softarch/demo/config/DBInitialization.java
+++ src/main/java/cat/udl/eps/softarch/demo/config/DBInitialization.java
@@ -1,20 +1,20 @@
 package cat.udl.eps.softarch.demo.config;
+
 import cat.udl.eps.softarch.demo.domain.Record;
+import cat.udl.eps.softarch.demo.domain.Tag;
 import cat.udl.eps.softarch.demo.domain.User;
 import cat.udl.eps.softarch.demo.repository.RecordRepository;
+import cat.udl.eps.softarch.demo.repository.TagRepository;
 import cat.udl.eps.softarch.demo.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import jakarta.annotation.PostConstruct;
-import cat.udl.eps.softarch.demo.domain.Tag;
-import cat.udl.eps.softarch.demo.repository.TagRepository;
+
 import java.time.ZonedDateTime;
 import java.util.Arrays;
 
-
 @Configuration
 public class DBInitialization {
-
 
     @Value("${default-password}")
     String defaultPassword;
@@ -26,8 +26,10 @@ public class DBInitialization {
     private final UserRepository userRepository;
     private final TagRepository tagRepository;
 
+    public DBInitialization(UserRepository userRepository,
+                            RecordRepository recordRepository,
+                            TagRepository tagRepository) {
 
-    public DBInitialization(UserRepository userRepository, RecordRepository recordRepository, TagRepository tagRepository) {
         this.userRepository = userRepository;
         this.recordRepository = recordRepository;
         this.tagRepository = tagRepository;
@@ -35,33 +37,46 @@ public class DBInitialization {
 
     @PostConstruct
     public void initializeDatabase() {
+
         // Default user
         if (!userRepository.existsById("demo")) {
+
             User user = new User();
-            user.setEmail("demo@sample.app");
             user.setId("demo");
+            user.setEmail("demo@sample.app");
             user.setPassword(defaultPassword);
             user.encodePassword();
+
             userRepository.save(user);
         }
+
+        // Testing instances
         if (Arrays.asList(activeProfiles.split(",")).contains("test")) {
-            // Testing instances
+
             if (!userRepository.existsById("test")) {
+
                 User user = new User();
-                user.setEmail("test@sample.app");
                 user.setId("test");
+                user.setEmail("test@sample.app");
                 user.setPassword(defaultPassword);
                 user.encodePassword();
+
                 user = userRepository.save(user);
-                cat.udl.eps.softarch.demo.domain.Record record = new Record();
+
+                Record record = new Record();
                 record.setName("My test record");
-                record.setDescription("A record used for testing purposes, nothing more, nothing less...");
+                record.setDescription(
+                    "A record used for testing purposes, nothing more, nothing less..."
+                );
                 record.setCreated(ZonedDateTime.now());
                 record.setModified(record.getCreated());
                 record.setOwnedBy(user);
+
                 recordRepository.save(record);
             }
         }
+
+        // Default tags
         if (!tagRepository.existsByName("Backend")) {
             tagRepository.save(new Tag("Backend"));
         }
